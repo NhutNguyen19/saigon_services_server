1a6bce749f591a8582b56d4f1c5962d291e3d967
NhutNguyen19 <nhujwt19@gmail.com>
2025-01-19 19:55:35 +0700

    the first configuration

---
+++ src/main/java/com/iuh/edu/fit/SaigonServicesServerApplication.java
@@ -0,0 +1,13 @@
+package com.iuh.edu.fit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaigonServicesServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SaigonServicesServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/iuh/edu/fit/SaigonServicesServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iuh.edu.fit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaigonServicesServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
